7c0823e464f0910bb6a1c9c5b9bc69db2a4182a3
Rachael Jane <rachinald@gmail.com>
2022-01-27 10:30:29 +0000

    intial

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const passport = require("passport");
+
+const connection = require("./connection");
+const User = require("./models/user");
+const userRouter = require("./routes/user");
+const {
+  registerStrategy,
+  loginStrategy,
+  verifyStrategy,
+} = require("./middleware/auth");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+// app.use(passport.initialize());
+//http://localhost/user/registeruser
+//{
+//  "name": "michael",
+//  "password": "lefgjdflibhg"
+//}
+
+//http://localhost/user/getallusers - sends request (req)
+app.use(cors());
+app.use("/user", userRouter);
+
+passport.use("register", registerStrategy);
+passport.use("login", loginStrategy);
+passport.use(verifyStrategy);
+
+app.listen(process.env.PORT, () => {
+  connection.authenticate();
+  User.sync({ alter: true });
+  console.log("App is online");
+});
